7c7603f91992e8c64f48e37b510beec6f70bb2a8
Carl Kadie <carlk@microsoft.com>
2014-12-12 10:50:21 -0800

    minor refactoring

--- sr/snp_gen.py
+++ sr/snp_gen.py
@@ -6,22 +6,6 @@ import logging
 import pysnptools.util as pstutil
 from pysnptools.snpreader import SnpData
 
-
-
-
-def write_plink(X): #!!!cmk rename
-    #import plink_write as pw
-    sample_names =  np.array(['s_%d' % i for i in range(X.shape[0])], dtype=str)
-    family_names = np.array(['f_%d' % i for i in range(X.shape[0])], dtype=str)[:, None]
-    paternal_ids = np.zeros_like(family_names,dtype=str)
-    maternal_ids = np.zeros_like(family_names,dtype=str)
-    sex = np.zeros_like(family_names,dtype=str)
-    fam_pid_mid_sex = np.concatenate((family_names, paternal_ids, maternal_ids, sex), axis=1)
-    snp_names=np.empty(X.shape[1],dtype='|S15')
-    for i in range(X.shape[1]):
-        snp_names[i] = 'snp_%d' %i
-    return sample_names, family_names, snp_names
-
 def snp_gen(fst, dfr, iid_count, sid_count, maf_low=.05, maf_high=.5, seed=0,sibs_per_family=10,freq_pop_1=.5): #!!!cmk move this to the front
     """
     #!!!cmk fill in with docs including example
@@ -39,22 +23,9 @@ def snp_gen(fst, dfr, iid_count, sid_count, maf_low=.05, maf_high=.5, seed=0,sib
 
 
     """
-    #!!!cmk want??? t0 = time.time()
-    #random.seed(seed) #!!!cmk still needed?
-    #np.random.seed(seed)
+    snps = _generate_data(num_snps=sid_count,randomseed=seed,fracSibs=dfr,numIndividuals=iid_count,num_children=sibs_per_family,pop_perc=freq_pop_1,maf_low=maf_low,maf_high=maf_high,fst=fst)
 
-    #self.FSTs = np.array([0.005, 0.01, 0.05, 0.1])
-    #self.fracSibs=np.array([0.0,0.05,0.1,0.2])
-    #self.FSTs, self.fracSibs, self.h2s, self.var_hidden, self.num_causal
-    #fst, fracSibs, h2, hh2, causal = params
-
-
-    snps = generate_data(num_snps=sid_count,randomseed=seed,fracSibs=dfr,numIndividuals=iid_count,num_children=sibs_per_family,pop_perc=freq_pop_1,maf_low=maf_low,maf_high=maf_high,fst=fst)
-
-
-    sample_names, family_names, sid = write_plink(snps)
-    iid = np.array(list(zip(sample_names,family_names[:,0])))
-    pos = np.array(list([i,0,0] for i in xrange(len(sid)))) # every snp has position 0,0 on its own chrom
+    iid, sid, pos =  _write_plink(snps)
 
     snpdata = SnpData(iid, sid, pos, snps, 
                       parent_string="snp_gen(fst={0}, dfr={1}, iid_count={2}, sid_count={3}, maf_low={4})".format(fst, dfr, iid_count, sid_count, maf_low) #!!!cmk is this up-to-date?
@@ -62,8 +33,7 @@ def snp_gen(fst, dfr, iid_count, sid_count, maf_low=.05, maf_high=.5, seed=0,sib
 
     return snpdata
 
-
-class sim_snps(object):
+class _sim_snps(object):
     """
     Generates genotypes with a certain MAF and optionally with population structure.
     In case of no population structure, they are sampled from a binomial,
@@ -192,12 +162,7 @@ class sim_snps(object):
         #snps_ = np.random.binomial(self.chr_copies,pgen,(sample_size,pgen.shape[0]))
         return snps
 
-
-
-
-
-
-def generate_data(num_snps, randomseed,fracSibs,numIndividuals,num_children,pop_perc,maf_low,maf_high,fst):
+def _generate_data(num_snps, randomseed,fracSibs,numIndividuals,num_children,pop_perc,maf_low,maf_high,fst):
     
     #set random seed
     np.random.seed(randomseed)
@@ -212,7 +177,7 @@ def generate_data(num_snps, randomseed,fracSibs,numIndividuals,num_children,pop_
     
 
     num_trios_pop= pop_perc*num_trios
-    simsnps = sim_snps(num_snps, num_differentiated = num_differentiated, MAF_ancestral = np.array([maf_low,maf_high]), Fst = Fst, p_ancestral=None)
+    simsnps = _sim_snps(num_snps, num_differentiated = num_differentiated, MAF_ancestral = np.array([maf_low,maf_high]), Fst = Fst, p_ancestral=None)
 
     snps_pop=[]
     i_parent_pop=[]
@@ -231,8 +196,22 @@ def generate_data(num_snps, randomseed,fracSibs,numIndividuals,num_children,pop_
     snps_all = np.concatenate([snps_all,snps_kids],0)
     return snps_all
 
+def _write_plink(X): #!!!cmk rename
+    #import plink_write as pw
+    sample_names =  np.array(['i_%d' % i for i in range(X.shape[0])], dtype=str)
+    family_names = np.array(['f_%d' % i for i in range(X.shape[0])], dtype=str)[:, None]
+    paternal_ids = np.zeros_like(family_names,dtype=str)
+    maternal_ids = np.zeros_like(family_names,dtype=str)
+    sex = np.zeros_like(family_names,dtype=str)
+    fam_pid_mid_sex = np.concatenate((family_names, paternal_ids, maternal_ids, sex), axis=1)
+    sid=np.empty(X.shape[1],dtype='|S15')
+    for i in range(X.shape[1]):
+        sid[i] = 'snp_%d' %i
 
+    iid = np.array(list(zip(sample_names,family_names[:,0])))
+    pos = np.array(list([i,0,0] for i in xrange(len(sid)))) # every snp has position 0,0 on its own chrom
 
+    return iid, sid, pos
 
 if __name__ == "__main__":
 
